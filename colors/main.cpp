a92b996514a1fb938ad710e8d869a67f7db18586
beulasamuel <beulathamban@gmail.com>
2021-02-28 09:33:35 +1300

    Initial Commit

---
+++ colors/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  colors
+//
+//  Created by Beula Samuel on 28/02/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
